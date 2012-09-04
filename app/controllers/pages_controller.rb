f59935889820108303fa5061a00f25c0d58d14a3
Keith Mackey <keith.mackey@gmail.com>
2012-09-04 15:01:05 -0500

    Initial Website

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  # GET /pages
+  # GET /pages.json
+  def index
+    @pages = Page.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @pages }
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoganSmith::Application.config.secret_token = '514f6a94103132c4884ce098fbbf0c5fe265f245e0ba656f01aa607f4674f6066df1025db66000a5c7cf5edaad09d4cfb82047313ed096fdfe62b6ba39e46df5'
